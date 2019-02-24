bd9aa93223754986ae08ca0f4feaa13d002363bd
Michael Reeves <michaelreeves808@gmail.com>
2019-02-24 02:52:46 -0800

    Added more tuning tools

--- Tests.py
+++ Tests.py
@@ -21,34 +21,35 @@ cap = cv2.VideoCapture(0)
 openVal = 1
 closeVal = 11
 erode = 10
-dilate = 0
-
-lowerBound=np.array([0,180,20])
-upperBound=np.array([20,255,255])
+dilate = 8
 
 cv2.namedWindow('image')
 
-def nothing(x):
-    pass
+def nothing(x): pass
 
 cv2.createTrackbar('Open','image',openVal,20,nothing)
 cv2.createTrackbar('Close','image',closeVal,40,nothing)
 cv2.createTrackbar('Erode','image',erode,30,nothing)
 cv2.createTrackbar('Dilate','image',dilate,30,nothing)
+cv2.createTrackbar('Saturation','image',170,255,nothing)
 
 while 1:
         
+    kernelOpen=np.ones((cv2.getTrackbarPos('Open','image'), cv2.getTrackbarPos('Open','image')))
+    kernelClose=np.ones((cv2.getTrackbarPos('Close','image'), cv2.getTrackbarPos('Close','image')))
+    kernelErode=np.ones((cv2.getTrackbarPos('Erode','image'), cv2.getTrackbarPos('Erode','image')))
+    kernelDilate=np.ones((cv2.getTrackbarPos('Dilate','image'), cv2.getTrackbarPos('Dilate','image')))
+    saturationMin = cv2.getTrackbarPos('Saturation','image')
+
+    lowerBound=np.array([0,saturationMin,20])
+    upperBound=np.array([20,255,255])
+
     ret, img = cap.read()
 
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lowerBound, upperBound)
 
 
-    kernelOpen=np.ones((cv2.getTrackbarPos('Open','image'), cv2.getTrackbarPos('Open','image')))
-    kernelClose=np.ones((cv2.getTrackbarPos('Close','image'), cv2.getTrackbarPos('Close','image')))
-    kernelErode=np.ones((cv2.getTrackbarPos('Erode','image'), cv2.getTrackbarPos('Erode','image')))
-    kernelDilate=np.ones((cv2.getTrackbarPos('Dilate','image'), cv2.getTrackbarPos('Dilate','image')))
- 
     mask=cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernelOpen, iterations = 1)
     mask=cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernelClose, iterations= 2)
     mask = cv2.erode(mask, kernelErode, iterations = 1)
@@ -56,14 +57,8 @@ while 1:
 
 
     maskImg = cv2.bitwise_and(img,img, mask= mask)
-
     maskImg = 255 - maskImg
-
     points = detector.detect(maskImg)
-
-    for i in points:
-        print(i.pt)
-
     img = cv2.drawKeypoints(img, points, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     cv2.imshow('image', maskImg)
